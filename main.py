501b1d74b71e81dbe1ee5dad8d7dc2a07c7c634a
rtmincic <mincic92@gmail.com>
2025-01-08 21:33:08 -0700

    Added fps and made it so you can click out of the game

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import pygame
 from constants import *
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
 def main():
-    pygame.init
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    while 1 > 0:
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
     
     
 
